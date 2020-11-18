d336a484dd0b7b4555fba53fd6bf0cbd258edcc5
Tareq Judeh <tareq.joudeh@gmail.com>
2020-11-18 15:07:31 +0000

    Adding ASCII text to project logos. Implementing while loops when the game is over.

--- main.py
+++ main.py
@@ -1,34 +1,87 @@
-from random import randint, choice
-from art import logo
-
-print(logo)
-
-guess_me = choice(range(1, 101))
-game_over = False
-attempts = 0
+from random import randint
+from art import main_logo, game_over_logo, you_win
+from replit import clear
 
 
-correct_input = False
-while correct_input == False:
 
-  difficulty = input("Choose game difficulty: Easy(10 attempts) / Hard(5 attempts)\n").lower()
-  if difficulty == "easy":
-    attempts = 10
-    correct_input = True
+
+def play_again(): 
+  restart_game = True
+  while restart_game:
+    if input("Play again? (y/n): ").lower() == "y":
+      clear()
+      guess_number()
+    else:
+      restart_game = False
+      print("Good Bye!")
+      return
+
+ 
+
+def guess_number():
+
+  print(main_logo)
+
+  attempts = 0
+  guess_me = randint(1, 100)
+
+  correct_input = False
+  game_over = False
+
+  while correct_input == False:
+    difficulty = input("Choose game difficulty: Easy(10 attempts) / Hard(5 attempts):\n").lower()
+    if difficulty == "easy":
+      attempts = 10
+      correct_input = True
+      
+    elif difficulty == "hard":
+      attempts = 5
+      correct_input = True
+      
+    else:
+      print("Bad input.")
+      print("")
+
+  print("")
+
+
+  while game_over == False:
+
+    if attempts == 0:
+      clear()
+      print(game_over_logo)
+      print(f"The Lucky Number is {guess_me}")
+      print('''
+      ''') 
+      game_over = True
+      return
+
+     
+      
+    print(f"Attemps left: {attempts}")
+    user_guess = int(input("Guess the Lucky Number: "))
+    print("") 
     
-  elif difficulty == "hard":
-    attempts = 5
-    correct_input = True
+    if user_guess == guess_me:
+      clear()
+      print(you_win)
+      print(f"The Lucky Number is {guess_me}")
+      print('''
+      ''')  
+      game_over = True
+      return
+
+    elif user_guess > guess_me:
+      print('''Your guess is HIGHER than the Lucky Number.
+      ''')
+      attempts -= 1
+    else:
+      print('''Your guess is LOWER than the Lucky Number.
+      ''')
+      attempts -= 1
     
-  else:
-    print("Bad input.")
+   
+guess_number()
 
 
-while game_over == False:
-
-  user_guess = input("Guess the Lucky Number: ")
-  if user_guess == guess_me:
-    print(f"Correct! you guessed {guess_me}! Which is the right number!")
-  elif user_guess > guess_me:
-    print("Your guess is higher than the Lucky Number")
-print(attempts)
+play_again()
